b8311ca38e55cb2303dc511495a4ca66637c223b
Fabiano Souza <fabianohensouza@gmail.com>
2022-03-09 22:12:43 -0300

    Starting the creation of migration and Database connection

--- core/Database.php
+++ core/Database.php
@@ -18,19 +18,40 @@ class Database
      */
     public function __construct(array $config) 
     {
-
-        $dsn = $config['dns'] ?? '';
+        $dsn = $config['dsn'] ?? '';
         $user = $config['user'] ?? '';
         $password = $config['password'] ?? '';
 
         try {
             $this->pdo = new \PDO($dsn, $user, $password);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
         catch(\PDOException $e) {
             die( 'Query failed: ' . $e->getMessage() );
-        }
+        } 
+    }
 
-        $this->pdo = new \PDO($dsn, $user, $password);
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    public function applyMigrations()
+    {        
+        $this->createMigrationsTable();
+        $this->getAppliedMigrations();
+    }
+
+    public function createMigrationsTable()
+    {        
+        $this->pdo->exec("  CREATE TABLE IF NOT EXISTS migrations (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                migration VARCHAR(255),
+                                created_at TIMESTAMP DEFAUL CURRENT_TIMESTAMP
+                            ) ENGINE=INNODB;"
+                        );
+    }
+
+    public function getAppliedMigrations()
+    {        
+        $statement = $this->pdo->prepare("SELEC migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
